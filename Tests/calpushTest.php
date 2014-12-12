3e7470cd5cd2d4bd02af9dfa2f2d4f7b3cd29222
Stefan Masztalerz <stefanmasz@hotmail.com>
2014-12-12 13:49:15 +0100

    adding phpunit

---
+++ Tests/calpushTest.php
@@ -0,0 +1,23 @@
+<?php
+require_once '../Src/class.calpush.php';
+
+
+/**
+ * Class test
+ */
+class calpushTest extends PHPUnit_Framework_TestCase{
+
+    /**
+     * test setup
+     */
+    public function setUp(){
+        $this->calpush = new calpush();
+    }
+
+    /**
+     * @test
+     */
+    public function pushcalendarTest(){
+
+    }
+}
